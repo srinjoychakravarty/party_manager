c7b05e6cafc541563dac05af37863d428e1a3c08
Srinjoy Chakravarty <chaxz93@gmail.com>
2012-04-29 21:01:30 +0300

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Party::Application.routes.draw do
 
-#  root :to => 'hosts#home' 
-map.root :controller => "hosts"
+  root :to => 'hosts#home' 
+
 #get "hosts/home"
 #match "/hosts/home" => "hosts#home" 
 match "hosts/new" => "hosts#new"
