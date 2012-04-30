188a5c0c46b5c3e7725f1747cc9dd5068b8d6b37
Srinjoy Chakravarty <chaxz93@gmail.com>
2012-04-30 06:40:04 +0300

    deleted

--- app/models/host.rb
+++ app/models/host.rb
@@ -1,12 +1,12 @@
 class Host < ActiveRecord::Base
-	email_regex = /[\w+\-.]+@[a-z\d\-.]+\.[a-z]+/i
+	#email_regex = /[\w+\-.]+@[a-z\d\-.]+\.[a-z]+/i
 	
 	attr_accessible :first_name, :last_name, :email
-	validates :first_name, :presence => true
-						   :length => { :maximum => 50 }
-	validates :last_name, :presence => true
-					      :length => { :maximum => 50 }
-	validates :email, :presence => true
-					  :format => { :with => email_regex} 
-					  :uniqueness => { :case_sensitive => false } 		
+	#validates :first_name, :presence => true
+	#					   :length => { :maximum => 50 }
+	#validates :last_name, :presence => true
+	#				      :length => { :maximum => 50 }
+	#validates :email, :presence => true
+	#				  :format => { :with => email_regex} 
+	#				  :uniqueness => { :case_sensitive => false } 		
 end
